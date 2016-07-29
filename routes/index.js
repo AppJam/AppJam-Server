0a704df72ff4f3689af88791105a158addaf38e4
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-07-29 13:40:33 -0600

    fix syntax error

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,28 @@ router.get('/', function(req, res, next) {
   })
 });
 
+router.get('/:id', function(req, res, next) {
+  return knex('prank').where('id', req.params.id).then(function(data) {
+    res.json(data);
+  })
+})
+
+router.post('/', function(req, res, next) {
+  return knex('prank').insert(req.body).then(function(data) {
+    res.json({success: "prank successfully added"});
+  })
+})
+
+router.put('/:id', function(req, res, next) {
+  return knex('prank').update(req.body).where('id', req.params.id).then(function(data) {
+    res.json({success: "item edited"});
+  })
+})
+
+router.delete('/:id', function(req, res, next) {
+  return knex('prank').del().where('id', req.params.id).then(function(data) {
+    res.json({success: "item deleted"});
+  })
+})
+
 module.exports = router;
